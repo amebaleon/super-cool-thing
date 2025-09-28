4031adda2b48983a738ee1a8842f17e645188102
amebaleon <windowleon@gmail.com>
2025-09-28 11:32:10 +0900

    헿헤

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/super-cool-thing/', // GitHub 저장소 이름에 맞춰 base 경로 설정
 })
